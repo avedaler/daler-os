ddae25feb1b0be503657f43ab182b5625fbbcc32
avedaler <avedaler@gmail.com>
2026-09-14 23:22:45 +0800

    feat: route /the-movie to mobile-first THE MOVIE V2

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import "./styles.css";
 import App from "./App";
+import MovieShell from "./components/MovieShell";
 
 registerSW({ immediate: true });
 
+const path = window.location.pathname.replace(/\/+$/, "");
+const isMovieRoute = path === "/the-movie" || path.endsWith("/the-movie");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isMovieRoute ? <MovieShell /> : <App />}
   </React.StrictMode>
 );
